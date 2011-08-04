853cd1286b23c9f5048418503e2559040fba958d
firman m <firman.alqoumy@gmail.com>
2011-08-04 23:57:47 +0700

    rev initial4

--- config/environment.rb
+++ config/environment.rb
@@ -30,7 +30,7 @@ Rails::Initializer.run do |config|
   config.gem 'colored', :version=> '1.1'
   config.gem 'youtube-g', :version=> '0.4.9.9', :lib=>'youtube_g'
   config.gem 'uuidtools', :version=> '1.0.4'
-  config.gem 'hpricot', :version=> '0.6.164'
+#  config.gem 'hpricot', :version=> '0.6.164'
   config.gem 'mocha', :version=> '0.9.3'
   config.gem 'redgreen', :version=> '1.2.2' unless ENV['TM_MODE']
   config.gem 'gcnovus-avatar', :version=> '0.0.7', :lib => 'avatar'
